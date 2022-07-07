b3b2f7d24e97c87230cf0ce1d2414fdc1130b116
ulormichael <ulormichael@gmail.com>
2022-07-07 17:02:14 +0100

    added add/main.js

---
+++ add/main.js
@@ -0,0 +1,4 @@
+function solution(a,b) {
+    return a+b;
+}
+console.log(solution(1,2));
---
+++ adjacentElementsProduct/main.js
@@ -0,0 +1,16 @@
+function solution(inputArray) {
+    var arr = inputArray;
+    var x = 0;
+    var y = 0;
+    var p = Number.MIN_SAFE_INTEGER;
+    for (var i = 0; i < arr.length; i++) {
+        x = arr[i];
+        y = arr[i + 1];
+        if (x * y > p) {
+            p = x * y;
+        };
+        return p;
+    };
+};
+
+console.log(solution([ -23, 4, -3, 8, -12]));
